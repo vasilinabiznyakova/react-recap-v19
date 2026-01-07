0c7ef9cfbf3d53f05d51dac519141cd559dfcca3
Vasylyna Bizniakova <vbiznyakova@gmail.com>
2026-01-07 18:31:02 +0100

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
+import axios from 'axios';
+
 import './HomePage.css';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => console.log(data));
-  
+  axios('http://localhost:3000/api/products').then((response) => {
+    response.data;
+    console.log(response.data);
+  });
+
   return (
     <>
       <title>Ecommerce Project</title>
